eb158cc6f12d26dc403d5ea85644e947e8b83526
Muhammed <muhammedd2129@gmail.com>
2025-05-10 22:53:25 +0300

    Computer Network SmartStructure project

---
+++ src/main/java/com/computernetwork/smartstructure/SmartstructureApplication.java
@@ -0,0 +1,13 @@
+package com.computernetwork.smartstructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartstructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartstructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/computernetwork/smartstructure/SmartstructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computernetwork.smartstructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
